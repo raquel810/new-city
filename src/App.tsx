8c46402be43b155454384944aef9720ff67ffd83
raquel810 <raquel@cabinettreestudio.com>
2026-08-03 15:45:41 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Layout from '@/components/Layout';
 import Home from '@/pages/Home';
 import DoorStyles from '@/pages/DoorStyles';
@@ -10,31 +9,20 @@ import Studio from '@/pages/Studio';
 import Contact from '@/pages/Contact';
 import ProjectNguyen from '@/pages/ProjectNguyen';
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-}
-
 function App() {
   return (
-    <>
-      <ScrollToTop />
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/door-styles" element={<DoorStyles />} />
-          <Route path="/finishes" element={<Finishes />} />
-          <Route path="/wood-species" element={<WoodSpecies />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/gallery/nguyen" element={<ProjectNguyen />} />
-          <Route path="/studio" element={<Studio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/door-styles" element={<DoorStyles />} />
+        <Route path="/finishes" element={<Finishes />} />
+        <Route path="/wood-species" element={<WoodSpecies />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/gallery/nguyen" element={<ProjectNguyen />} />
+        <Route path="/studio" element={<Studio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 
